e1a6ab411528916731f5c8be3227c394779c5ac7
Ahmad <Ahmad.Badwan@axsos.academy.com>
2025-12-07 13:42:21 +0200

    first jsx

---
+++ mern/3/First JSX/firstJsx/src/components/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Home=()=>{
+    return(
+        <>
+            <h1>Hello Dojo !</h1>
+            <h2>Thinges I need to do :</h2>
+            <li>
+                <ul>learn react</ul>
+                <ul>climp Mt Everest</ul>
+                <ul>Run a marathon </ul>
+                <ul>Feed the dogs</ul>
+            </li>
+        </>
+    );
+}
+export default Home;
